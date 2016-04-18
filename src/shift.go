6f4c57f15b277611a1a55427dd0e0d016a9af140
EC2 Default User <ec2-user@ip-172-31-38-89.us-west-2.compute.internal>
2016-04-18 21:17:08 +0000

    block drop effect, fixes

--- src/shift.go
+++ src/shift.go
@@ -14,40 +14,46 @@ import (
 )
 
 func shift() {
-	src := decode("smut.png")
+	src := decode("smut")
 	bounds := src.Bounds()
 	w, h := bounds.Max.X, bounds.Max.Y
 	out := image.NewRGBA(bounds)
 	//top_color_list := getTopColors(src, 3)
-	//randC1 := randColor()
-	//randC2 := randColor()
+	randC1 := randColor()
+	randC2 := randColor()
 	drip(src, out, 1, 0, 0.8)
-	for i := 0; i < 50; i++ {
-		drip(out, out, 1, 0, 0.8)
-	}
-	for i := 0; i < 25; i++ {
-		drip(out, out, -1, 3, 0.95)
-	}
 	for ix := 0; ix < w; ix++ {
 		for iy := 0; iy < h; iy++ {
 			cont := contrast(src, ix, iy)
-			if cont > 0.001 && cont < 0.015 {
-				//out.Set(ix, iy, randC1)
+			if ix%100 == 0 && iy%100 == 0 {
+				fmt.Print("contrast at ", ix, ", ",
+					iy, " = ", cont, "\n")
 			}
-			if ix%50 == 0 && iy%50 == 0 {
-				//fmt.Print("contrast at ", ix, ", ", iy, " = ", cont, "\n")
-				//fmt.Print("color diff of ", colorDiff(src.At(ix, iy), src.At(ix+1, iy)), "\n")
+			if cont < 0.01 {
+				out.Set(ix, iy, colorCloserTo(src, ix, iy, randC1))
+			}
+			if cont > 0.01 && cont < 0.018 {
+				out.Set(ix, iy, colorCloserTo(src, ix, iy, randC2))
 			}
 			if iy < 20 {
-				//out.Set(ix, iy, randC1)
+				out.Set(ix, iy, randC1)
 			}
 			if iy < 40 && iy > 20 {
-				//out.Set(ix, iy, randC2)
+				out.Set(ix, iy, randC2)
 			}
 		}
 	}
-	transposeX(src, out, 70, 320, 15)
-	transposeY(src, out, 100, randY(src), randY(src))
+	blockDrip(src, out, 0, -1, 40, 30, 0.0005)
+	blockDrip(src, out, -1, -1, 20, 10, 0.0008)
+	transposeX(src, out, 100, randX(src), randX(src))
+	transposeY(src, out, 90, randY(src), randY(src))
+	transposeY(src, out, 60, randY(src), randY(src))
+	/*
+		p1 := image.Point{5, 5}
+		p2 := image.Point{200, 178}
+		c := src.At(1, 1)
+		drawSolidRect(out, image.Rectangle{p1, p2}, c)
+	*/
 	//encode the outfile and close
 	outfile, err := os.Create("out.png")
 	if err != nil {
@@ -95,14 +101,31 @@ func drip(in image.Image, out *image.RGBA, ltr int, ttb int, degree float64) {
 			if rand.Float64() < degree {
 				//the pixel to drip from the in image
 				if isInBounds(out, x+ltr, y+ttb) {
-					if x%100 == 0 && y%100 == 0 {
-						//fmt.Print("dripping pixel at ", x, ", ", y, "\n")
-					}
 					out.Set(x, y, in.At(x+ltr, y+ttb))
 				}
 			} else {
 				if isInBounds(out, x, y) {
 					out.Set(x, y, in.At(x, y))
+				}
+			}
+		}
+	}
+}
+
+//block is the width of sections to drip
+func blockDrip(in image.Image, out *image.RGBA, ltr int, ttb int,
+	blockw int, blockh int, degree float64) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	w, h := in.Bounds().Max.X, in.Bounds().Max.Y
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			if rand.Float64() < degree {
+				for bx := 0; bx < blockw; bx++ {
+					for by := 0; by < blockh; by++ {
+						if isInBounds(out, x+ltr+bx, y+ttb+by) {
+							out.Set(x+ltr+bx, y+ttb+by, out.At(x, y))
+						}
+					}
 				}
 			}
 		}
@@ -134,8 +157,17 @@ func transposeY(img image.Image, out *image.RGBA, n int, offset1 int, offset2 in
 	}
 }
 
-func drawSolidRect(img image.Image, rect Rectangle) {
-
+//p is the top left-most pixel where the rectangle will be drawn
+func drawSolidRect(img *image.RGBA, rect image.Rectangle, c color.Color) {
+	c = c.(color.RGBA)
+	w, h := img.Bounds().Max.X, img.Bounds().Max.Y
+	for x := rect.Min.X; x < w; x++ {
+		for y := rect.Min.Y; y < h; y++ {
+			if isInBounds(img, x, y) {
+				img.Set(x, y, c)
+			}
+		}
+	}
 }
 
 func brightness(img image.Image, x int, y int) uint8 {
@@ -186,6 +218,15 @@ func invertColor(c color.RGBA) color.RGBA {
 		uint8(math.Abs(float64(b - 255))),
 		255}
 	return inverted
+}
+
+func iterate(out *image.RGBA, foo func(c color.RGBA)) {
+	w, h := out.Bounds().Max.X, out.Bounds().Max.Y
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			foo(out.At(x, y).(color.RGBA))
+		}
+	}
 }
 
 //returns a list of colors after iterating through the image and collecting
@@ -274,10 +315,14 @@ func colorCloserTo(img image.Image, x int, y int, newColor color.RGBA) color.RGB
 	return color.RGBA{nr, ng, nb, 255}
 }
 
+//where filename is the name of the file without it's type
 func decode(filename string) image.Image {
+	//concatenate the filename (not performant)
+	filename = "img/" + filename + ".png"
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Print(err)
+		panic(err)
 	}
 	defer file.Close()
 	src, _, err := image.Decode(file)
@@ -293,10 +338,11 @@ func test() {
 	//img := decode("jihadi-john.png")
 	//getColorsListFuzzy(img, 0.5)
 	//getTopColors(img, 5)
+
 }
 
 func main() {
-	cleanEnv()
+	//cleanEnv()
 	shift()
 	//test()
 }
